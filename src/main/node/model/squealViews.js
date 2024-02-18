e60583459e1bebeaff9a2c421ed4367dc2b8224b
Matteo Raggi <matteo.raggi04@gmail.com>
2024-02-18 12:03:10 +0100

    feat: added user_id to views

--- src/main/node/model/squealViews.js
+++ src/main/node/model/squealViews.js
@@ -3,6 +3,7 @@ const { Schema, model, ObjectId } = require('mongoose');
 const squealViewsSchema = new Schema(
   {
     squeal_id: { type: String },
+    user_id: { type: String },
     number: { type: Number, default: 0 },
   },
   { collection: 'squeal_views', _id: true }
